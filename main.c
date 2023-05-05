6f61920eb3be6877c053ab5576147baae459b9c9
Lars Przybylek <l.p.1999@live.de>
2023-05-05 23:11:37 +0200

    created basic framework

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <stdbool.h>
 #include <malloc.h>
-
-
+#define MAX_PATH_LEANGTH 60
+enum datatyp {NONE,CSV,JSON,XML};
 
 struct Node{
     struct Node* next;
@@ -33,8 +33,13 @@ char* popNode(struct Node* queqe){
         return NULL;
     while (pos->next!=NULL)
         pos = pos->next;
-    return pos->data;
+    char* out = pos->data;
+    free(pos);
+    pos = NULL;
+    return out;
 }
+
+
 
 
 
@@ -78,11 +83,35 @@ void readCsv(FILE* in){
     }
 }
 
+void readerCSV(FILE* file,struct Node* head){
+
+}
+void readerJSON(FILE* file,struct Node* head){
+
+}
+void readerXML(FILE* file,struct Node* head){
+
+}
+void writerCSV(FILE* file,struct Node* head){
+
+}
+void writerJSON(FILE* file,struct Node* head){
+
+}
+void writerXML(FILE* file,struct Node* head){
+
+}
+
+
+void (*reader[])(FILE* file,struct Node* head) = {NULL,readerCSV,readerJSON,readerXML};
+
+void (*writer[])(FILE* file,struct Node* head) = {NULL,writerCSV,writerJSON,writerXML};
+
 int main(int argc, char** argv)
 {
-    const unsigned int maxPathLeangth = 60;
-    char inPath[maxPathLeangth];
-    char outPath[maxPathLeangth];
+
+    char inPath[MAX_PATH_LEANGTH];
+    char outPath[MAX_PATH_LEANGTH];
 
     if (!getPaths(argc, argv, inPath, outPath)){
         fprintf(stderr,"\nPlease provide an input an output parameter");
@@ -97,6 +126,33 @@ int main(int argc, char** argv)
     char* inDataTyp = strtok(NULL,".");
     strtok(outPath,".");
     char* outDataTyp = strtok(NULL,".");
+
+    struct Node* head = (struct Node*) malloc(sizeof(struct Node));
+    initQueqe(head);
+
+    if(!strcmp(inDataTyp,"csv")){
+        reader[CSV](inFile,head);
+    } else if(!strcmp(inDataTyp,"json")) {
+        reader[JSON](inFile,head);
+    } else if(!strcmp(inDataTyp,"xml")) {
+        reader[XML](inFile,head);
+    } else {
+        reader[NONE](inFile,head);
+    }
+
+
+    if(!strcmp(outDataTyp,"csv")){
+        writer[CSV](inFile,head);
+    } else if(!strcmp(outDataTyp,"json")) {
+        writer[JSON](inFile,head);
+    } else if(!strcmp(outDataTyp,"xml")) {
+        writer[XML](inFile,head);
+    } else {
+        writer[NONE](inFile,head);
+    }
+
+
+
 
 
     return 0;
